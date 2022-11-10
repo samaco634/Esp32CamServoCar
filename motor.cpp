519c57b3bc890a41f56567e9981ee3c438a36d26
samaco634 <72288990+samaco634@users.noreply.github.com>
2022-11-10 20:55:35 +0900

    Update motor.cpp

--- motor.cpp
+++ motor.cpp
@@ -5,30 +5,46 @@
 Servo servo1;
 int currentDegree ;
 
+void set_servo90(void)
+{
+  if(int pos = servo1.read() < 90)
+  {
+    for (pos = servo1.read(); pos < 90; pos += 2) {
+      servo1.write(pos);
+      delay(40);
+    }    
+  }
+  else
+  {
+    for (int pos = servo1.read(); pos > 90; pos -= 2) {
+      servo1.write(pos);
+      delay(40);
+    }    
+  }
+   servo1.write(90);
+}
+
 void motor_begin(void)
 {
   pinMode(MOTOR_PIN_1, OUTPUT);
   pinMode(MOTOR_PIN_2, OUTPUT);
   
   servo1.attach(SERVO_PIN);
-  currentDegree = 90;
+  servo1.write(90);
 }
 void motor_forward(int speed)
 {
+    set_servo90();
     analogWrite(MOTOR_PIN_1, speed);
     analogWrite(MOTOR_PIN_2, 0);
-    currentDegree = 90;
-    servo1.write(currentDegree);
-
 }
 
 void motor_backward(int speed)
 {
+    set_servo90();
     analogWrite(MOTOR_PIN_1, 0);
     analogWrite(MOTOR_PIN_2, speed);
-  
-    currentDegree = 90;
-    servo1.write(currentDegree);
+
 }
 
 void motor_left(int speed)
@@ -36,8 +52,10 @@ void motor_left(int speed)
     analogWrite(MOTOR_PIN_1, speed);
     analogWrite(MOTOR_PIN_2, 0);
 
-    currentDegree = 50;
-    servo1.write(currentDegree);
+    for (int pos = servo1.read(); pos > 70; pos -= 2) {
+      servo1.write(pos);
+      delay(40);
+    }
 }
 
 void motor_right(int speed)
@@ -45,14 +63,15 @@ void motor_right(int speed)
     analogWrite(MOTOR_PIN_1, speed);
     analogWrite(MOTOR_PIN_2, 0);
 
-    currentDegree = 130;
-    servo1.write(currentDegree);
+    for (int pos = servo1.read(); pos < 110; pos += 2) {
+      servo1.write(pos);
+      delay(40);
+    }
 }
 
 void motor_stop(void)
 {
-    currentDegree = 90;
-    servo1.write(currentDegree);
+    set_servo90(); 
     analogWrite(MOTOR_PIN_1, 0);
     analogWrite(MOTOR_PIN_2, 0);
 }
